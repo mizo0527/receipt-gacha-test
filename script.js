72b678280f1d6f63b9ecb94651a2754898613dfc
mizo0527 <mn1234.luckythirteen@gmail.com>
2026-07-09 20:10:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert("script.js 読み込みOK！");
+async function startCamera() {
+
+    try {
+
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: {
+                facingMode: "environment"
+            }
+        });
+
+        document.getElementById("camera").srcObject = stream;
+
+    } catch (err) {
+
+        alert("カメラが起動できませんでした。");
+
+        console.error(err);
+
+    }
+
+}
